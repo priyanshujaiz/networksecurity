bab1fd4a539e8a2a1b402f27790c11309fc2be5d
Priyanshu Ranjan <priyanshujaiz341@gmail.com>
2025-12-06 09:25:03 +0530

    Model evaluation and mlfow initialisation

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig,DataValidationConfig,DataTransformationConfig
-
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.components.data_validation import DataValidation
 
 
@@ -19,12 +19,14 @@ if __name__=='__main__':
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiation Completed")
         print(dataingestionartifact)
+        print('\n')
         data_validation_config=DataValidationConfig(trainingpipelineconfig)
         data_validation=DataValidation(data_validation_config=data_validation_config,data_ingestion_artifact=dataingestionartifact)
         logging.info("Initiate the data validation")
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("Data Validation Completed")
         print(data_validation_artifact)
+        print('\n')
 
         #Data Transformation
         data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
@@ -32,10 +34,17 @@ if __name__=='__main__':
         data_transformation=DataTransformation(data_transformation_config=data_transformation_config,data_validation_artifact=data_validation_artifact)
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
+        print('\n')
         logging.info("Data Transformation Completed")
         
-
-
+        logging.info("Initiate the model trainer")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logging.info("Model Trainer Completed")
+        print(model_trainer_artifact)
+        print('\n')
+        
         
     except Exception as e:
            raise NetworkSecurityException(e,sys)
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -16,6 +16,9 @@ TEST_FILE_NAME: str = "test.csv"
 
 SCHEMA_FILE_PATH: str=os.path.join("data_schema","schema.yaml")
 
+SAVED_MODEL_DIR: str =os.path.join("saved_models")
+MODEL_FILE_NAME: str = "model.pkl"
+
 
 """
 Data Ingestion realted constant start with DATA_INGESTION VAR NAME
@@ -47,6 +50,19 @@ DATA_TRANSFORMATION_IMPUTER_PARAMS:dict={
     "n_neighbors":3,
     "weights":"uniform",
 }
+DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
+
+DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
 
 
+
+
+
+"Model Trainer related constant start with MODEL_TRAINER VAR NAME"
+
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD: float = 0.05
 
